da1a79209cfa3bddc475ddc1d2012bec10cdcbb4
Rupesh Ojha <rupeshojha@hotmail.com>
2023-04-05 15:22:14 +0530

    comment

---
+++ MVCCoreWithViewController/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCCoreWithViewController.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index(string userName, string country)
+        {
+            ViewBag.UserName = userName;
+            return View();
+        }
+    }
+}
